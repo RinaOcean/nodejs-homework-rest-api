055a0824fc98a9c57215aab5570f23d3b3e7453a
RinaOcean <antyfifa@gmail.com>
2021-08-05 23:56:53 +0300

    add sendMail.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const logger = require('morgan')
 const cors = require('cors')
 
 
+
+
 const contactsRouter = require('./routes/api/contacts.router')
 const userAuthRouter = require('./routes/api/user.auth.router')
 const usersRouter = require('./routes/api/users.router')
---
+++ utils/sendMail/sendMail.js
@@ -0,0 +1,26 @@
+const sgMail = require('@sendgrid/mail')
+require('dotenv').config()
+
+const { SENDGRID_API_KEY } = process.env
+
+sgMail.setApiKey(SENDGRID_API_KEY)
+
+const sendMail = async ({ to, subject, text, html }) => {
+  const mail = {
+    to,
+    from: 'katerynatorkachenko@gmail.com',
+    subject,
+    text,
+    html,
+  }
+
+  try {
+    const answear = await sgMail.send(mail)
+    return answear
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+}
+
+module.exports = sendMail
